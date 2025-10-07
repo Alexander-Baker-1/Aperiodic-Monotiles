eead9a4a7a56bd74aec4317867c3ff12f95da322
Alexander Baker <Alexander.Baker-1@colorado.edu>
2025-10-06 18:50:25 -0600

    Skeleton files

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/continuum')
+def continuum():
+    return render_template('continuum.html')
+
 app.run("0.0.0.0", debug=True)
